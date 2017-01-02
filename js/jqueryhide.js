3e8c349038bc40513cfbf8c63110d83add0376b8
kevin dhenin <dhenin.k@laposte.net>
2017-01-02 16:01:41 +0100

    Rajout du bonhomme de neige et popup

---
+++ js/jqueryhide.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+	jQuery("#logopop").click(function(){
+		jQuery(this).hide();
+	});
+	jQuery("#snowflakeContainer").click(function(){
+		jQuery(this).hide();
+	});
+});
